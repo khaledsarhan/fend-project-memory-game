e4d512d95798ed3dc7f8b6887557e7d505d8936e
Khaled Sarhan <khaled_sarhan2003@yahoo.com>
2018-03-31 13:00:33 +0300

    Added the matching functionality to the cards

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 /*
  * Create a list that holds all of your cards
  */
+let openedCards = [];
+let matchedCards = 0;
+
 let cardSymbols = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube",
     "fa-leaf", "fa-bicycle", "fa-bomb", "fa-diamond", "fa-paper-plane-o",
     "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
-    
+
 cardSymbols = shuffle(cardSymbols);
 /*
  * Display the cards on the page
@@ -21,6 +24,7 @@ function displayCards() {
     for (let i = 0; i < cardSymbols.length; i++) {
         let cardItem = document.createElement("li");
         cardItem.classList.add("card");
+        cardItem.classList.add("close");
 
         let cardContent = document.createElement("li");
         cardContent.classList.add("fa");
@@ -48,6 +52,43 @@ function shuffle(array) {
     return array;
 }
 
+function showSymbol(openedCard) {
+    openedCard.setAttribute('class', 'card open');
+    openedCards.push(openedCard);
+}
+
+function setOpenedCardsClasses(classes) {
+    openedCards.forEach(function (card) {
+        card.setAttribute('class', classes);
+    });
+}
+
+function checkSymbols() {
+    if (openedCards.length === 2) {
+
+        document.querySelector(".deck").removeEventListener('click', cardClick)
+        setTimeout(function () {
+            openedCards = [];
+            document.querySelector(".deck").addEventListener('click', cardClick)
+        }, 700);
+
+        if (openedCards[0].firstChild.classList[1] === openedCards[1].firstChild.classList[1]) {
+            setOpenedCardsClasses('card match');
+
+            matchedCards++;
+            if (matchedCards === cardSymbols.length / 2) {
+                // Completed
+            }
+
+        } else {
+            setOpenedCardsClasses('card mismatch');
+            setTimeout(function () {
+                setOpenedCardsClasses('card close')
+            }, 500);
+        }
+    }
+}
+
 displayCards();
 
 /*
@@ -60,3 +101,11 @@ displayCards();
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+document.querySelector(".deck").addEventListener('click', cardClick)
+
+function cardClick(e) {
+    if (e.target.classList.contains('card') && !e.target.classList.contains('match') && !e.target.classList.contains('open')) {
+        showSymbol(e.target);
+        checkSymbols();
+    }
+}
